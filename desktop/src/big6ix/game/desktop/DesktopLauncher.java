ae203980fd4630e3fb46778e63041aec2a3d1ccd
Michael Szalewicz <szalewicz.m@gmail.com>
2017-10-21 19:34:02 +0200

    Set up project files

---
+++ desktop/src/big6ix/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package big6ix.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import big6ix.game.GameMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GameMain(), config);
+	}
+}
