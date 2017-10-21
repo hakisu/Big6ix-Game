86bf08c9c5705704cfcd5b1b4a8a59a677c288c8
Michael Szalewicz <szalewicz.m@gmail.com>
2017-10-22 00:18:08 +0200

    Configure basic project settings

--- core/src/big6ix/game/GameMain.java
+++ core/src/big6ix/game/GameMain.java
@@ -7,22 +7,36 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class GameMain extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+	private SpriteBatch batch;
+	private Texture img;
+	private long oldTime;
+	private long ticksPerSecond = 60;
+	private long timeAccumulator = 0;
+//	frameTime represents time reserved for one frame in nanoseconds
+	private long frameTime = 1000000000 / ticksPerSecond;
+
 	@Override
 	public void create () {
+		System.out.println("from create start");
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		oldTime = System.nanoTime();
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+//        System.out.println(String.format("%12f",Gdx.graphics.getRawDeltaTime()));
+        long newTime = System.nanoTime();
+        long timeDifference = newTime - oldTime;
+        oldTime = newTime;
+        timeAccumulator += timeDifference;
+
+        if(timeAccumulator >= 1000000000) {
+            System.out.println(timeDifference / 1000000.0);
+            timeAccumulator = 0;
+        }
+
+        updateGraphics();
 	}
 	
 	@Override
@@ -30,4 +44,16 @@ public class GameMain extends ApplicationAdapter {
 		batch.dispose();
 		img.dispose();
 	}
+
+	private void updateGraphics() {
+	    Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        batch.draw(img, 0, 0);
+        batch.end();
+    }
+
+    private void updatePhysics() {
+
+    }
 }
--- desktop/src/big6ix/game/desktop/DesktopLauncher.java
+++ desktop/src/big6ix/game/desktop/DesktopLauncher.java
@@ -7,6 +7,17 @@ import big6ix.game.GameMain;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.fullscreen = false;
+        config.foregroundFPS = 0;
+        config.backgroundFPS = 0;
+        config.vSyncEnabled = false;
+        config.x = -1;
+        config.y = -1;
+        config.width = 800;
+        config.height = 400;
+        config.useGL30 = true;
+        config.title = "Game-No title yet";
+
 		new LwjglApplication(new GameMain(), config);
 	}
 }
