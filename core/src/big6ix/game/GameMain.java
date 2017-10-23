9c326e78c53429d6f9a383bb0c500745245029ca
Michael Szalewicz <szalewicz.m@gmail.com>
2017-10-23 14:15:41 +0200

    Add basic game functionality

--- core/src/big6ix/game/GameMain.java
+++ core/src/big6ix/game/GameMain.java
@@ -2,58 +2,104 @@ package big6ix.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.*;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class GameMain extends ApplicationAdapter {
-	private SpriteBatch batch;
-	private Texture img;
-	private long oldTime;
-	private long ticksPerSecond = 60;
-	private long timeAccumulator = 0;
-//	frameTime represents time reserved for one frame in nanoseconds
-	private long frameTime = 1000000000 / ticksPerSecond;
+    private Texture img;
+    private Texture playerImage;
+    private SpriteBatch batch;
+    private OrthographicCamera camera;
 
-	@Override
-	public void create () {
-		System.out.println("from create start");
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-		oldTime = System.nanoTime();
-	}
+    //	time management fields
+    private long oldTime;
+    private final long ticksPerSecond = 60;
+    private long timeAccumulator = 0;
+    // frameTime represents time reserved for one frame in nanoseconds
+    private long frameTime = 1000000000 / ticksPerSecond;
 
-	@Override
-	public void render () {
-//        System.out.println(String.format("%12f",Gdx.graphics.getRawDeltaTime()));
+    // temp fields
+    private float posX = 20;
+    private float posY = 100;
+    private float speed = 0.1f;
+
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        playerImage = new Texture("player.png");
+        camera = new OrthographicCamera();
+        camera.setToOrtho(true, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+
+        Cursor gameCursor = Gdx.graphics.newCursor(new Pixmap(Gdx.files.internal("crosshair.png")), 16, 16);
+        Gdx.graphics.setCursor(gameCursor);
+
+        oldTime = System.nanoTime();
+    }
+
+    @Override
+    public void render() {
         long newTime = System.nanoTime();
         long timeDifference = newTime - oldTime;
         oldTime = newTime;
         timeAccumulator += timeDifference;
 
-        if(timeAccumulator >= 1000000000) {
+        if (timeAccumulator >= 1000000000) {
             System.out.println(timeDifference / 1000000.0);
             timeAccumulator = 0;
         }
 
-        updateGraphics();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+        handleInput();
+        updatePhysics();
+        updateGraphics(timeDifference);
+    }
 
-	private void updateGraphics() {
-	    Gdx.gl.glClearColor(0, 0, 0, 1);
+    @Override
+    public void dispose() {
+        playerImage.dispose();
+        batch.dispose();
+        System.out.println("Dispose was successful!");
+    }
+
+    private void updateGraphics(long timeDifference) {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        camera.position.x = posX + 32;
+        camera.position.y = posY + 32;
+
+        camera.update();
+        batch.setProjectionMatrix(camera.combined);
+
         batch.begin();
-        batch.draw(img, 0, 0);
+        batch.draw(playerImage, posX, posY);
+        batch.draw(playerImage, 200, 100);
         batch.end();
     }
 
     private void updatePhysics() {
+    }
 
+    private void handleInput() {
+        // movement commands
+        if (Gdx.input.isKeyPressed(Input.Keys.W)) {
+            posY -= speed;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.S)) {
+            posY += speed;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.D)) {
+            posX += speed;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.A)) {
+            posX -= speed;
+        }
+        if (Gdx.input.isTouched()) {
+            System.out.println(Gdx.input.getX() + "  " + Gdx.input.getY());
+        }
+        // exit command
+        if (Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE)) {
+            Gdx.app.exit();
+        }
     }
 }
--- desktop/src/big6ix/game/desktop/DesktopLauncher.java
+++ desktop/src/big6ix/game/desktop/DesktopLauncher.java
@@ -1,23 +1,23 @@
 package big6ix.game.desktop;
 
+import big6ix.game.GameMain;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import big6ix.game.GameMain;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.fullscreen = false;
+    public static void main(String[] arg) {
+        System.out.println(LwjglApplicationConfiguration.getDesktopDisplayMode().toString());
+
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.setFromDisplayMode(LwjglApplicationConfiguration.getDesktopDisplayMode());
+
+        config.title = "Game-No title yet";
+        config.fullscreen = true;
         config.foregroundFPS = 0;
         config.backgroundFPS = 0;
         config.vSyncEnabled = false;
-        config.x = -1;
-        config.y = -1;
-        config.width = 800;
-        config.height = 400;
-        config.useGL30 = true;
-        config.title = "Game-No title yet";
+        config.forceExit = true;
 
-		new LwjglApplication(new GameMain(), config);
-	}
+        new LwjglApplication(new GameMain(), config);
+    }
 }
