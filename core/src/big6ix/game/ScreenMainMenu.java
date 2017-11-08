bc28b172d9f39364509b0c6fb56982ea3f53a890
dteterwak <32641112+dteterwak@users.noreply.github.com>
2017-11-08 17:17:46 +0100

    Add main menu using Scene2d

--- core/src/big6ix/game/ScreenMainMenu.java
+++ core/src/big6ix/game/ScreenMainMenu.java
@@ -1,56 +1,90 @@
 package big6ix.game;
 
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
 import com.badlogic.gdx.ScreenAdapter;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+
 
 public class ScreenMainMenu extends ScreenAdapter {
-    private final GameMain gameMain;
-    private Color fontColor;
 
-    private OrthographicCamera camera;
+    final GameMain gameMain;
+    private SpriteBatch batch;
+    protected Stage stage;
+    private TextureAtlas atlas;
+    protected Skin skin;
 
-    public ScreenMainMenu(GameMain gameMain) {
-        this.gameMain = gameMain;
-
-        fontColor = Color.BLACK;
-        gameMain.font.setColor(fontColor);
-        gameMain.font.getData().setScale(1.0f);
-
-        camera = new OrthographicCamera();
-        camera.setToOrtho(true, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+    public ScreenMainMenu(final GameMain game)
+    {
+        this.gameMain = game;
+        //ustawienie skins
+        atlas = new TextureAtlas("uiskin.atlas");
+        skin = new Skin(Gdx.files.internal("uiskin.json"), atlas);
+        batch = new SpriteBatch();
+        stage = new Stage();
     }
 
-    // odpala sie kiedy ten ekran staje sie aktywny
+
     @Override
     public void show() {
-        System.out.println("MainMenu screen set to active.");
+        Gdx.input.setInputProcessor(stage);
+
+        //Tworzenie tabeli
+        Table mainTable = new Table();
+        mainTable.setFillParent(true);
+        //Ustawienie miejsca
+        mainTable.center();
+
+        //Tworzenie buttonów
+        TextButton playButton = new TextButton("Play", skin);
+        TextButton optionsButton = new TextButton("Options", skin);
+        TextButton exitButton = new TextButton("Exit", skin);
+
+        //Obsługa kliknięć
+        playButton.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                ((Game)Gdx.app.getApplicationListener()).setScreen(gameMain.screenGame);
+            }
+        });
+        exitButton.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                Gdx.app.exit();
+            }
+        });
+
+        //Dodanie buttonów do tabeli
+        mainTable.add(playButton);
+        mainTable.row();
+        mainTable.add(optionsButton);
+        mainTable.row();
+        mainTable.add(exitButton);
+
+        //Dodanie tabeli do stage
+        stage.addActor(mainTable);
     }
 
-    // glowna petla, ktora wykonywana jest caly czas dopoki dany ekran jest aktywny
     @Override
     public void render(float delta) {
-        gameMain.batch.setProjectionMatrix(camera.combined);
-
-        Gdx.gl.glClearColor(0.3f, 0.5f, 1, 1);
+        Gdx.gl.glClearColor(.1f, .12f, .16f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        // tymczasowy sposob aby odpalic gre
-        if (Gdx.input.isKeyJustPressed(Input.Keys.SPACE)) {
-            gameMain.setScreen(gameMain.screenGame);
-        }
+        stage.act();
+        stage.draw();
+    }
 
-        // tymczasowe wejscie do ekranu settings
-        if (Gdx.input.isKeyJustPressed(Input.Keys.ENTER)) {
-            gameMain.setScreen(gameMain.screenSettings);
-        }
-
-        // wyjscie z gry
-        if (Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE)) {
-            Gdx.app.exit();
-        }
+    @Override
+    public void dispose() {
+        skin.dispose();
+        atlas.dispose();
     }
 }
